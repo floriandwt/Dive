b93b619570d9604ff41fcaf941770c3d436852ed
flooorianflo <florian.kiem@hfg.design>
2023-06-05 14:01:06 +0200

    Dashboard

---
+++ dive/data/groups.ts
@@ -0,0 +1,16 @@
+import { Group } from "../types";
+
+/**
+ * This array simulates a database consisting of a list of groups.
+ * Assign users to groups in /data/users.ts
+ */
+export const groups: Group[] = [
+  {
+    id: "dive",
+    name: "Dives",
+  },
+  {
+    id: "old",
+    name: "Old Dives",
+  },
+];
