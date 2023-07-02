ebf5b419bdd250568d71ded5dc5e3bd7ac696653
flooorianflo <florian.kiem@hfg.design>
2023-07-02 02:01:57 +0200

    new platform

--- source/server.js
+++ source/server.js
@@ -1,15 +1,50 @@
 // make express available for ./src/index.html and send data to the client
 const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { cors: { origin: "*" } });
 
-
-// Path: ./src/index.html
 const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 
-// start the server on port 3000
-app.listen(3000, 
-  // '172.21.20.30', 
-  () => {
-  console.log("Server started on port 3000");
+let waitlist = [];
+let guide = null;
+let guest = null;
+
+io.on("connection", (socket) => {
+  socket.on("enter", (content) => {
+    if (content.msg === "guide" && guide === null) {
+      guide = socket.id;
+    } else if (content.msg === "guest" && guest === null) {
+      guest = socket.id;
+      socket.emit("enterReply", "success");
+    } else if (content.msg === "guest" && guest !== null) {
+      waitlist.push(socket.id);
+
+      socket.emit("enterReply", "waitlist");
+    } else if (content.msg === "guide-success") {
+      console.log("guide-success");
+      socket.emit("planningDone", "dive-computer");
+    }
+  });
+
+  socket.on("disconnect", () => {
+    if (socket.id === guide) {
+      guide = null;
+    } else if (socket.id === guest) {
+      guest = null;
+      if (waitlist.length > 0) {
+        guest = waitlist[0];
+        waitlist = waitlist.slice(1);
+        io.to(guest).emit("enterReply", "success");
+      }
+    } else {
+      waitlist = waitlist.filter((id) => id !== socket.id);
+    }
+  });
 });
+
+httpServer.listen(3000);
